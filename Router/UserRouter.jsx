576a74772e20999a955371ceef77ef3fbd0f98d5
sarah <minipouce75@gmail.com>
2022-01-05 15:58:54 +0100

    add new files MVC

---
+++ Router/UserRouter.jsx
@@ -0,0 +1,6 @@
+const router = require ("express").Router();
+const mysql = require ('../db');
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+ 
 
 const server = app.listen(SERVER_PORT, () => {
   console.log(`Server is listening on : ${SERVER_PORT}`);
