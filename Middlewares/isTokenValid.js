2f5ac0e788db3cb4e8e93d91018c39dee9456005
Eti-Fromentin <fromentin.etienne@gmail.com>
2022-01-17 10:47:38 +0100

    user and auth route finished

--- Middlewares/isTokenValid.js
+++ Middlewares/isTokenValid.js
@@ -3,20 +3,22 @@ const jwt = require('jsonwebtoken');
 const { UnAuthorizedError } = require('./errors/errors-types');
 
 const isTokenValid = (req, res, next) => {
+  const id = req.params.id;
+  console.log(id);
   if (!req.headers.authorization) {
     throw new UnAuthorizedError();
-  } else {
-    console.log(req.headers.authorization);
-    const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, SECRET, (err, decoded) => {
-      if (err) {
-        console.error(decoded);
-        throw new UnAuthorizedError();
-      } else {
-        next();
-      }
-    });
   }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, SECRET, (err, decoded) => {
+    if (err) {
+      console.error(decoded);
+      throw new UnAuthorizedError();
+    }
+    const decodedToken = jwt.decode(token, { complete: true });
+    if (decodedToken.payload.id !== Number(id)) {
+      throw new UnAuthorizedError();
+    } else next();
+  });
 };
 
 module.exports = isTokenValid;
