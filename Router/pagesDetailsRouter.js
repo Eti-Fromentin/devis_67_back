9e87a5f08c059834b65d4db8c579b144f59ad211
Eti-Fromentin <fromentin.etienne@gmail.com>
2022-01-26 15:39:12 +0100

    get dynamic route enabled

--- Router/pagesDetailsRouter.js
+++ Router/pagesDetailsRouter.js
@@ -3,5 +3,6 @@ const asyncHandler = require('express-async-handler');
 const pagesDetailsController = require('../Controller/pagesDetailsController');
 
 router.get('/:name', asyncHandler(pagesDetailsController.getPagesDetails));
+router.get('/', asyncHandler(pagesDetailsController.getDevisDynamicRoutes));
 
 module.exports = router;
