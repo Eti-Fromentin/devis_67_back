114d0aa136f94e821da2642bd51c25225ddaff40
Eti-Fromentin <fromentin.etienne@gmail.com>
2022-01-27 18:21:37 +0100

    get category link route done

--- Router/homeDevisRouter.js
+++ Router/homeDevisRouter.js
@@ -3,5 +3,5 @@ const asyncHandler = require('express-async-handler');
 const homeDevisController = require('../Controller/homeDevisController');
 
 router.get('/', asyncHandler(homeDevisController.getAll));
-
+router.get('/categories', asyncHandler(homeDevisController.getCategLink));
 module.exports = router;
